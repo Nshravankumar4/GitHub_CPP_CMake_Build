2d07d5d496d93b3c8fe44b87fab4d008a65b48a8
Shravan Kumar Nunsavath <93977456+Nshravankumar4@users.noreply.github.com>
2025-10-12 15:53:27 +0530

    Add files via upload

--- tests/test_hello.cpp
+++ tests/test_hello.cpp
@@ -1,1 +1,15 @@
 
+#include <iostream>
+#include "../include/hello.h"  // Include the header for getMessage()
+
+int main() {
+    std::string msg = getMessage();
+
+    if (msg == "Hello from CI/CD C++ project!") {
+        std::cout << "Test Passed!" << std::endl;
+        return 0; // success
+    } else {
+        std::cout << "Test Failed!" << std::endl;
+        return 1; // failure
+    }
+}
